c7682fbf62d32c45aa73a42ec956bd15486aa544
Angel Concepcion <angelconcepcion50@gmail.com>
2022-07-19 14:35:46 -0400

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,10 @@ public class Main {
     static String SUFFIX = "__suffix__";
 	
 	// fix the operator code
-    public static UnaryOperator<String> operator = str -> PREFIX + str + SUFFIX;
+    public static UnaryOperator<String> operator = str -> PREFIX + str.replaceAll(" ", "") + SUFFIX;
 
     public static void main(String[] args) {
-        String res = operator.apply("hello");
+        String res = operator.apply("  hello");
         System.out.println(res); // __prefix__hello__suffix__
     }
 }
